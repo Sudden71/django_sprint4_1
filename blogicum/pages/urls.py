8069ea3eca83ea0c054b25a33d8f9d824a43926b
Ruslan <Ruslan71zzz.rus@gmail.com>
2026-03-31 00:46:09 +0300

    Исправлены все замечания: профиль, comment_count, фильтрация, пагинация, права доступа

---
+++ blogicum/pages/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = 'pages'
+
+urlpatterns = [
+    path('auth/registration/', views.register, name='registration'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+]
